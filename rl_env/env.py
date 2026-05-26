ae9ab7a800263f4ce70a6a4bb41391a18e596dc0
Le Roy Hugo <hleroyh@gmail.com>
2026-05-26 10:50:17 +0200

    first commit for rl environment

---
+++ rl_env/env.py
@@ -0,0 +1,4 @@
+"""
+This class creates an environment that rl agents can interact with.
+It launch the genesis env, and create a robot and enable udp connection between them : unvisible to the agent.
+"""
